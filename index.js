dfa8a033108d5d0933ca9be6895b347288c9ed78
leonbaraka1-debug <leon.baraka1@student.moringaschool.com>
2026-07-02 11:41:53 +0300

    This is a lab showing how modules are used

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+require('datejs');
 
+function combineUsers(...args) {
+  const combinedObject = {
+    users: []
+  };
+
+  // Step 3: loop through args
+  args.forEach(function(userArray) {
+    // Step 4: merge with spread
+    combinedObject.users = [...combinedObject.users, ...userArray];
+  });
+
+  // Step 5: today's date
+  combinedObject.merge_date = Date.today().toString('M/d/yyyy');
+
+  // Step 7: return
+  return combinedObject;
+}
+
+module.exports = combineUsers;
 
 
 module.exports = {
